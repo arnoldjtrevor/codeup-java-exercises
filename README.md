07bcdb37eaa922bddc376b01845a6f5b582da9e8
Trevor Arnold <arnold.j.trevor@gmail.com>
2020-08-24 09:46:40 -0500

    Finished Java Syntax, Data Types, and Variables exercise

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 A brief overview of the lessons and exercises from the Codeup curriculum that helped me build and strengthen my Java skills.  
 ## Java I
 ### Introduction To Java
-In this lesson, we learned about the history of Java and discussed the different editions of Java. 
+In this lesson, I learned about the history of Java and discussed the different editions of Java. 
 
-Our exercise this lesson had us write a simple Hello World program using Java. You can find this exercise at: 
+Our exercise this lesson had me write a simple Hello World program using Java. You can find this exercise at: 
 
 [HelloWorld.java](src/HelloWorld.java)
+
+### Java Syntax, Data Types, and Variables
+In this lesson, I learned about basic Java syntax, the fundamental data types, and simple Java variables.
+
+The exercise this lesson had me print int
+
 
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,84 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+// Exercise One
+
+    // TODO Create a program that outputs "Hello, World!" to the console.
+
+        // System.out.println("Hello, World!");
+
+// Exercise Two
+
+    // TODO: Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+
+        // int myFavoriteNumber = 13;
+        // System.out.print("myFavoriteNumber: " + myFavoriteNumber + "\n");
+
+    // TODO: Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+
+        // String myString = "Rihanna Release An Album Challenge, Do That Challenge";
+        // System.out.print(myString);
+
+    // TODO: Change your code to assign a character value to myString. What do you notice?
+
+        // It don't work :(
+
+    // TODO: Change your code to assign the value 3.14159 to myString. What happens?
+
+        // String myString = "3.14159";
+        // System.out.print(myString);
+
+        // It displays the number
+
+    // TODO: Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+
+        // long myNumber;
+        // System.out.print(myNumber);
+
+        // Displays: Error:(35, 26) java: variable myNumber might not have been initialized
+
+    // TODO: Change your code to assign the value 3.14 to myNumber. What do you notice?
+
+        // long myNumber = 3.14;
+        // System.out.print(myNumber);
+
+        // Displays: Error:(41, 25) java: incompatible types: possible lossy conversion from double to long
+
+   // TODO: Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+
+        // long myNumber = 123L;
+        // System.out.print(myNumber);
+
+   // TODO: Change your code to assign the value 123 to myNumber.
+
+        // long myNumber = 123;
+        // System.out.print(myNumber);
+
+   // TODO: Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+
+        // 3.14 needs to be a float because it has a decimal
+
+   // TODO: Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+
+        // float myFloatNumber = 3.14f;
+        // System.out.print(myFloatNumber);
+
+   // TODO: Copy and paste the following code blocks one at a time and execute them
+
+        // int x = 5;
+        // System.out.println(x++);
+        // System.out.println(x);
+
+        // int x = 5;
+        // System.out.println(++x);
+        // System.out.println(x);
+
+    // TODO: What is the difference between the above code blocks? Explain why the code outputs what it does.
+
+        // 1. The incrementation happens after the value of x is printed
+        // 2. The incrementation happens before the value of x is printed
+
+    // TODO: Try to create a variable named class. What happens?
+
     }
 }
